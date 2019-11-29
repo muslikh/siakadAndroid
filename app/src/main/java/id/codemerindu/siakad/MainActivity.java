72b7bebb5ba485201ff8ad9ce57e71c2da9ff154
muslikh <muslikh32@gmail.com>
2019-11-29 22:39:14 +0700

    Update Gan

--- app/src/main/java/id/codemerindu/siakad/MainActivity.java
+++ app/src/main/java/id/codemerindu/siakad/MainActivity.java
@@ -2,9 +2,15 @@ package id.codemerindu.siakad;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,7 +20,19 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity
+        implements BaseSliderView.OnSliderClickListener,
+        ViewPagerEx.OnPageChangeListener{
+
+private SliderLayout sliderShow;
 
     Button btn_logout;
     TextView txt_id, txt_username;
@@ -36,8 +54,103 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setSubtitle("SMK NEGERI PRIGEN");
         toolbar.setLogo(R.mipmap.ic_logo);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.aksesMenu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId())
+                {
+                    case  R.id.beranda:
+                        Toast.makeText(getApplication(),"Beranda",Toast.LENGTH_LONG).show();
+                        break;
+                    case  R.id.grup:
+                        break;
+                    case  R.id.profil:
+                        break;
+                }
+                return false;
+            }
+        });
+
         txt_username = (TextView) findViewById(R.id.txt_username);
-        btn_logout = (Button) findViewById(R.id.btn_logout);
+//        btn_logout = (Button) findViewById(R.id.btn_logout);
+//        btn_logout.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                // TODO Auto-generated method stub
+//                // update login session ke FALSE dan mengosongkan nilai id dan username
+//                SharedPreferences.Editor editor = sharedpreferences.edit();
+//                editor.putBoolean(Login.session_status, false);
+//                editor.putString(TAG_ID, null);
+//                editor.putString(TAG_USERNAME, null);
+//                editor.commit();
+//
+//                Intent intent = new Intent(MainActivity.this, Login.class);
+//                finish();
+//                startActivity(intent);
+//            }
+//        });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_main);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        NavigationView navigationView = findViewById(R.id.drawer);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId())
+                {
+                    case R.id.logout:
+
+                                // update login session ke FALSE dan mengosongkan nilai id dan username
+                                SharedPreferences.Editor editor = sharedpreferences.edit();
+                                editor.putBoolean(Login.session_status, false);
+                                editor.putString(TAG_ID, null);
+                                editor.putString(TAG_USERNAME, null);
+                                editor.commit();
+
+                                Intent intent = new Intent(MainActivity.this, Login.class);
+                                finish();
+                                startActivity(intent);
+
+                        break;
+
+                }
+
+                return false;
+            }
+        });
+
+        sliderShow = (SliderLayout) findViewById(R.id.slider);
+
+        HashMap<String, String> url_maps = new HashMap();
+        url_maps.put("Bengong", "https://lh3.googleusercontent.com/rQPEV7eJZZNU_1clOKBnHIzZZMaD_rgKebi3OJEGVH6oURVodWnVrtXMMhidN5JvuJg=h310");
+        url_maps.put("Melongo", "https://i0.wp.com/www.amazine.co/wp-content/uploads/2013/12/Kucing_1.jpg");
+        url_maps.put("Apaaa", "http://islamidia.com/wp-content/uploads/2016/07/Kucing-dan-Kedudukannya-Dalam-Pandangan-Islam.jpg");
+        url_maps.put("Bobo", "https://hellosehat.com/wp-content/uploads/2016/11/tidur-dengan-kucing.jpg");
+
+        //-- looping image stored
+        for(String name : url_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(this);
+            textSliderView
+                    .description(name)
+                    .image(url_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra", name);
+
+            sliderShow.addSlider(textSliderView);
+        }
+        sliderShow.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        sliderShow.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        sliderShow.setCustomAnimation(new DescriptionAnimation());
+        sliderShow.setDuration(3000);
+        sliderShow.addOnPageChangeListener(this);
+
 
         sharedpreferences = getSharedPreferences(Login.my_shared_preferences, Context.MODE_PRIVATE);
 
@@ -47,25 +160,11 @@ public class MainActivity extends AppCompatActivity {
 //        txt_id.setText("ID : " + id);
 //        txt_username.setText("USERNAME : " + username);
 
-        btn_logout.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                // update login session ke FALSE dan mengosongkan nilai id dan username
-                SharedPreferences.Editor editor = sharedpreferences.edit();
-                editor.putBoolean(Login.session_status, false);
-                editor.putString(TAG_ID, null);
-                editor.putString(TAG_USERNAME, null);
-                editor.commit();
-
-                Intent intent = new Intent(MainActivity.this, Login.class);
-                finish();
-                startActivity(intent);
-            }
-        });
 
     }
+
+
 
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -92,5 +191,32 @@ public class MainActivity extends AppCompatActivity {
 
         }
         return false;
+    }
+
+    @Override
+    protected void onStop() {
+        sliderShow.stopAutoCycle();
+        super.onStop();
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(this, slider.getBundle().get("extra") + "",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        Log.e("Slider Demo", "Page Changed: " + position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
