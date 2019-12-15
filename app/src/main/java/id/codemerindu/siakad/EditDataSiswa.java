3a94b4320996f439a2da097af096db83fabae027
mmuslikh <4lfa404@gmail.com>
2019-12-15 22:14:54 +0700

    penambahan edit siswa

--- app/src/main/java/id/codemerindu/siakad/EditDataSiswa.java
+++ app/src/main/java/id/codemerindu/siakad/EditDataSiswa.java
@@ -3,9 +3,13 @@ package id.codemerindu.siakad;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -17,12 +21,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EditDataSiswa extends AppCompatActivity {
 
     String url = Server.URL +"siswa.php";
+    String url_update  = Server.URL+"update.php";
     final String TAG ="Edit";
     public final static String TAG_IDU = "idu";
+    public final static String TAG_MESSAGE = "message";
     EditText EdnamaUser,EdttlUser,EdkodeKelas,Edjurusan;
+    Button updateSiswa;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +49,14 @@ public class EditDataSiswa extends AppCompatActivity {
         EdttlUser = (EditText) findViewById(R.id.ttlUser);
         EdkodeKelas = (EditText) findViewById(R.id.kodeKelasUser);
         Edjurusan = (EditText)  findViewById(R.id.jurusanUser);
+        updateSiswa = (Button)  findViewById(R.id.updateSiswa);
+        updateSiswa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                simpan();
+            }
+        });
+
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         StringRequest stringRequests =
                 new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
@@ -58,10 +78,10 @@ public class EditDataSiswa extends AppCompatActivity {
                                 String jurusanS = obj.getString("kode_jurusan");
                                 if (extraId== id )
                                 {
-                                    EdnamaUser.setHint(nama);
-                                    EdttlUser.setHint(tempatLahir +","+ tanggalLahir);
-                                    EdkodeKelas.setHint(kodekelas);
-                                    Edjurusan.setHint(jurusanS);
+                                    EdnamaUser.setText(nama);
+                                    EdttlUser.setText(tempatLahir +","+ tanggalLahir);
+                                    EdkodeKelas.setText(kodekelas);
+                                    Edjurusan.setText(jurusanS);
                                 }
                             }
                             Log.d(TAG, "onResponse:" + response);
@@ -79,5 +99,52 @@ public class EditDataSiswa extends AppCompatActivity {
                     }
                 });
         requestQueue.add(stringRequests);
+    }
+
+    private void simpan()
+    {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url_update, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject dataObj = new JSONObject(response);
+
+
+                    Toast.makeText(EditDataSiswa.this, dataObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+                    // adapter.notifyDataSetChanged();
+
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.getMessage());
+                Toast.makeText(EditDataSiswa.this, error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+
+            @Override
+
+            protected Map<String,String> getParams() throws AuthFailureError {
+
+                Map<String,String> map = new HashMap<>();
+                //map.put("id_siswaBaru", extraId);
+                map.put("nama", EdnamaUser.getText().toString());
+                map.put("kode_jurusan", Edjurusan.getText().toString());
+                map.put("kelas", EdkodeKelas.getText().toString());
+                // params.put("tahun_nmasuk", thnmasuk.getText().toString());
+                //}
+                return map;
+            }
+
+        };
+
+        AppController.getInstance().addToRequestQueue(stringRequest);
     }
 }
